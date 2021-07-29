03f43be8ead11f9f0e9d9c895e3197f53e342c86
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-29 14:45:48 -0500

    updated

--- routes/login.js
+++ routes/login.js
@@ -29,7 +29,7 @@ router.post('/', function(req, res, next) {
                 })
         })
 
-    res.json({message: 'success'})
+    // res.json({message: 'success'})
 })
 
 module.exports = router;
