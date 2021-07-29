504c65068cd587cc3f84ab43354281d127a5e396
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-07-29 13:30:58 -0500

    updated

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,8 @@
 const signUpForm = document.getElementById('')
-const emailInput = document.getElementById('')
-const passwordInput = document.getElementById('')
+const emailInputSignUp = document.getElementById('')
+const passwordInputSignUp = document.getElementById('')
+const firstNameInputSignUp = document.getElementById('')
+const lastNameInputSignUp = document.getElementById('')
 
 // addEventListener for submit to the form
+signUpForm.addEventListener
