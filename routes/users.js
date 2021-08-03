ec74bbebb1c2ade092a7c15c7b370cb573402043
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-08-03 15:31:09 -0500

    updating the overhaul

--- routes/users.js
+++ routes/users.js
@@ -24,8 +24,6 @@ router.post('/login', function (req, res, next) {
         }
     })
         .then((user) => {
-            // console.log('found user')
-            // console.log(user)
             // todo 
                 // * if user doesn't exist itll be a NULL 
                 // * make an if function that if NULL => incorrect user or/and password is incorrect
@@ -39,7 +37,6 @@ router.post('/login', function (req, res, next) {
                         req.session.user = user
                         res.redirect('/')
                     } else {
-                        // res.status(401).json({ error: 'incorrect password' })
                         req.flash('error', 'Wrong password')
                         res.redirect('/users/login')
                     }
@@ -59,7 +56,6 @@ router.get('/login/guest', (req, res) => {
         .then((user) => {
             if (user) {
                 req.session.user = user
-                // console.log(req.session.user)
                 res.redirect('/')
             }
         })
@@ -67,7 +63,6 @@ router.get('/login/guest', (req, res) => {
 
 // GET home page.
 router.get('/signup', function (req, res, next) {
-    // console.log('test')
     res.render('signup', { 
         title: 'Express',
         messages: req.flash()
@@ -88,28 +83,18 @@ router.post('/signup', function (req, res, next) {
                 email: req.body.email,
                 password: hash
             })
-                // respond with success and rout to the search page 
                 .then((user) => {
                     req.flash('success', 'Account successfully created')
-                    // res.status(201).json(user)
                     // todo redirect after submit to the results page
                     res.redirect('/users/signup')
                 })
         })
     }
-    // res.json({message: 'success'})
 })
 
 router.get('/logout', (req, res) => {
-    // tell express user logged out 
     req.session.user = null;
     res.redirect('/users/login')
-    // send response of successfully  
 })
-
-router.get('/test', function(req, res) {
-        db.Favorite.sync({force: true})
-        res.json("testing")
-    })
 
 module.exports = router;
