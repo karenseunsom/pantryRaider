c748fe9fb7c47493c68389481367d79ecb7040dd
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-08-03 19:59:41 -0500

    work in progress

--- checkAuth.js
+++ checkAuth.js
@@ -4,17 +4,8 @@ function checkAuth(req, res, next) {
     } else if (req.path == '/users/login' || req.path == '/users/signup') {
         next();
     } else {
-        // res.status(401).json({error: 'Not Logged In'})
         res.redirect('/users/login')
     }
 }
 
 module.exports = checkAuth
-
-
-
-// todo 
-// 1) make a post route for /favorites that will create a new line for user and a new fav dish
-// 2) check if user is signed in
-// 3) create a new db line
-// 4) make the table for favorites dish
