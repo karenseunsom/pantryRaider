eb97aee42821edf6939a4d07b04473f9fcab9bdf
Joey Zelinka <cheflife365@gmail.com>
2021-08-05 09:18:44 -0400

    Final Countdown

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,15 +2,20 @@ var express = require("express");
 var router = express.Router();
 const db = require('../models')
 
+
 router.get("/", function (req, res, next) {
-    res.render("favorites", { title: "Express" });
+    db.Favorite.findAll({
+        where:{UserId:req.session.user.id},
+        include:[db.Dish]
+    })
+    .then(favorites=>{
+        res.render("favorites", { title: "Express", favorites });
+
+    })
 });
 
 
 
-router.post("/:id/add",(req, res)=>{
-    db.Recipe.findOrCreate({
-        where: {}
-    })
-})
+
+
 module.exports=router
